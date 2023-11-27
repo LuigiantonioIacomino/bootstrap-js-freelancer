8483b2b3f779b395f0afe74af895982cad012fa1
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2023-11-27 12:26:09 +0100

    lavoro finito

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let prezzoCommissione="";
 let PrezzoFinaleScreen=document.getElementById('prezzoFinale');
+let ButtonSubmit=document.getElementById('invia');
 
 let arrayCodiciSconto=["YHDNU32","JANJC63","PWKCN25","SJDPO96","POCIE24"];
 
@@ -15,32 +16,47 @@ Form.addEventListener("submit",function(event) {
         if(arrayCodiciSconto.includes(codiceSconto)) {
         prezzoCommissione=oreLavoro*15.30;
         prezzoCommissione*=0.25;
+        ButtonSubmit.style.backgroundColor="yellow";
         }
         else{
-            console.log("codice inserito non valido")
-          prezzoCommissione=oreLavoro*15.30;
+            prezzoCommissione=oreLavoro*15.30;
+            console.log("codice inserito non valido");
+            ButtonSubmit.style.backgroundColor="red";
+
         }
     }
     else if(typeOfWork=="Back-End") {
         if(arrayCodiciSconto.includes(codiceSconto)) {
             prezzoCommissione=oreLavoro * 20.50
             prezzoCommissione*=0.25;
+            ButtonSubmit.style.backgroundColor="yellow";
             }
             else{
               prezzoCommissione=oreLavoro*20.50;
               console.log("codice inserito non valido")
+              ButtonSubmit.style.backgroundColor="red";
             }
     }
     else if(typeOfWork=="Project Analisys") {
         if(arrayCodiciSconto.includes(codiceSconto)) {
             prezzoCommissione=oreLavoro * 33.60;
             prezzoCommissione*=0.25;
+            ButtonSubmit.style.backgroundColor="yellow";
             }
             else{
             prezzoCommissione=oreLavoro * 33.60;
             console.log("codice inserito non valido")
+            ButtonSubmit.style.backgroundColor="red";
+
             }
     }
     PrezzoFinaleScreen.innerHTML=prezzoCommissione.toFixed(2)+"€";
+    for (let i = 0; i < arrayCodiciSconto.length; i++) {
+        if(arrayCodiciSconto[i]==codiceSconto) {
+            arrayCodiciSconto.splice(i,1);
+        }
+        
+    }
 })
+
 
