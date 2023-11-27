0b19db9f5db982f61ee1cd4833410d6dbe096151
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2023-11-27 11:57:19 +0100

    .

--- js/script.js
+++ js/script.js
@@ -1,24 +1,46 @@
 let prezzoCommissione="";
 let PrezzoFinaleScreen=document.getElementById('prezzoFinale');
-let oreLavoro=document.getElementById("input0").value;
-oreLavoro=parseInt(oreLavoro);
+
+let arrayCodiciSconto=["YHDNU32","JANJC63","PWKCN25","SJDPO96","POCIE24"];
+
 
 let Form=document.getElementById("form");
 Form.addEventListener("submit",function(event) {
     event.preventDefault();
+    let codiceSconto=document.getElementById("input5").value;
     let typeOfWork=document.getElementById('seleziona').value;
-    console.log(typeOfWork);
+    let oreLavoro=document.getElementById("input0").value;
+    oreLavoro=parseInt(oreLavoro);
     if(typeOfWork=="Front-end") {
-        prezzoCommissione=oreLavoro;
-        console.log(prezzoCommissione);
+        if(arrayCodiciSconto.includes(codiceSconto)) {
+        prezzoCommissione=oreLavoro*15.30;
+        prezzoCommissione*=0.25;
+        }
+        else{
+            console.log("codice inserito non valido")
+          prezzoCommissione=oreLavoro*15.30;
+        }
     }
     else if(typeOfWork=="Back-End") {
-        prezzoCommissione=oreLavoro * 20.50;
+        if(arrayCodiciSconto.includes(codiceSconto)) {
+            prezzoCommissione=oreLavoro * 20.50
+            prezzoCommissione*=0.25;
+            }
+            else{
+              prezzoCommissione=oreLavoro*20.50;
+              console.log("codice inserito non valido")
+            }
     }
     else if(typeOfWork=="Project Analisys") {
-        prezzoCommissione=oreLavoro * 33.60;
+        if(arrayCodiciSconto.includes(codiceSconto)) {
+            prezzoCommissione=oreLavoro * 33.60;
+            prezzoCommissione*=0.25;
+            }
+            else{
+            prezzoCommissione=oreLavoro * 33.60;
+            console.log("codice inserito non valido")
+            }
     }
-
+    PrezzoFinaleScreen.innerHTML=prezzoCommissione.toFixed(2)+"€";
 })
 
-PrezzoFinaleScreen.innerHTML=prezzoCommissione;
